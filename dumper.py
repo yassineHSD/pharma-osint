5b67d72b5dd610e0d92c7288ff52809692005577
sudoer <sudoer@desktop-j50t5pl.home>
2022-01-13 11:53:56 +0100

    outputs data to json

--- dumper.py
+++ dumper.py
@@ -8,7 +8,6 @@ import json
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-
 url_list=[]
 
 nltk.download('vader_lexicon')
@@ -30,23 +29,26 @@ def qualify(comment,ss):
 def process_comments(comments):
     processed_data=[]
     for comment in comments:
-        sid = SentimentIntensityAnalyzer()
-        ss = sid.polarity_scores(comment["text"])
-        Qualification=qualify(comment["text"],ss)
-        print(comment["dateModified"])
-        print(comment["text"])
-        print(Qualification)
-        print("_____________________________")
-        row={
-        "Comment":comment["text"],
-        "dateModified":comment["dateModified"],
-        "Qualification":Qualification,
-        "upvoteCount":comment["upvoteCount"],
-        "username":comment["author"]["name"],
-        "author.type":comment["author"]["@type"],
-        "url":comment["url"],
-        }
-        processed_data.append(row)
+        if "text" in comment:
+            sid = SentimentIntensityAnalyzer()
+            ss = sid.polarity_scores(comment["text"])
+            Qualification=qualify(comment["text"],ss)
+            print(comment["dateModified"])
+            print(comment["text"])
+            print(Qualification)
+            print("_____________________________")
+            row={
+            "Comment":comment["text"],
+            "dateModified":comment["dateModified"],
+            "Qualification":Qualification,
+            "upvoteCount":comment["upvoteCount"],
+            "username":comment["author"]["name"],
+            "author.type":comment["author"]["@type"],
+            "url":comment["url"],
+            }
+            processed_data.append(row)
+        else:
+            continue
     return processed_data
 
 driver_path = "/usr/local/bin/chromedriver"
@@ -84,41 +86,25 @@ f = open("url_list.txt", "w")
 f.write(str(url_list))
 f.close()
 
-#Now we scrape the comments
-
 myheader = {"User-Agent": "GoogleBot"}
-#url="https://amp.reddit.com/r/SecurityAnalysis/comments/5zhbwq/roche_vs_novartis_profitability/"
-#session1 = tor_session()
 
+final_output=[]
 for url in url_list:
+    print(url)
     session = tor_session()
-    url=url.replace("www.","amp.")
+    url=url.replace("//www.","//amp.")
     r = session.get(url)
     body=r.text
 
     pattern_json = "\<script type\=\"application\/ld\+json\"\>(.*?)\<\/script\>"
-    json_data = re.search(pattern_json, body).group()[35:-9]
+
+    if re.search(pattern_json, body):
+        json_data = re.search(pattern_json, body).group()[35:-9]
 
     if "comment" in json.loads(json_data):
         comments = json.loads(json_data)["comment"]
-        output=process_comments(comments)
-        with open('output.json', 'w') as jsondumpfile:
-            json.dump(output, jsondumpfile)
+        final_output=final_output+process_comments(comments)
     else:
         print("No comments found in this reddit/Can not load comments from this reddit")
-
-#session = requests.Session()
-#r = session.get(url)
-#body=r.text
-'''
-pattern_json = "\<script type\=\"application\/ld\+json\"\>(.*?)\<\/script\>"
-json_data = re.search(pattern_json, body).group()[35:-9]
-
-if "comment" in json.loads(json_data):
-    comments = json.loads(json_data)["comment"]
-    output=process_comments(comments)
-    with open('output.json', 'w') as jsondumpfile:
-        json.dump(output, jsondumpfile)
-else:
-    print("No comments found in this reddit/Can not load comments from this reddit")
-'''
+with open('output.json', 'a') as jsondumpfile:
+    json.dump(final_output, jsondumpfile,indent=4)
